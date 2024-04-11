cd3d970da700ece91886a0254ef1b140bc8e9ad1
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-11 21:55:21 +0300

    collect entity component types (test)

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,31 @@
 import * as vscode from 'vscode';
+import { isScalar, isCollection, parseDocument, visit } from 'yaml';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"robust-yaml" is now active!');
 	let disposable = vscode.languages.registerHoverProvider('yaml', {
 		provideHover(document, position, token) {
+			let doc = parseDocument(document.getText());
+			let contents: string[] = [];
+			visit(doc, {
+				Map(_, map) {
+					if (map.get('type') === 'entity') {
+						let components = map.get('components');
+						if (isCollection(components)) {
+							visit(components, {
+								Pair(_, pair) {
+									if (isScalar(pair.key) && pair.key.value === 'type' && isScalar(pair.value)) {
+										contents.push(pair.value.toString());
+									}
+								}
+							});
+						}
+					}
+					return visit.SKIP;
+				}
+			});
 			return {
-			  contents: ['Hover Content']
+			  contents: contents
 			};
 		}
 	});
