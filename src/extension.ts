d6b8ce8ce05cb7240e0b660ae72f1286c674a3c0
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-11 21:05:54 +0300

    hover test

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,14 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"robust-yaml" is now active!');
+	let disposable = vscode.languages.registerHoverProvider('yaml', {
+		provideHover(document, position, token) {
+			return {
+			  contents: ['Hover Content']
+			};
+		}
+	});
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
