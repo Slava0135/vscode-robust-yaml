27cdec00bd1a8976b0624b49dab752719ad19e8e
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-12 15:45:00 +0300

    more specific uri (search in shared files too)

--- src/file/uri.ts
+++ src/file/uri.ts
@@ -1,5 +1,5 @@
 export function containsComponentDefinition(uri: string, name: string): boolean {
-    const str = `.*${name}Component\.cs`.toLowerCase();
+    const str = `.*/(Shared)?${name}Component\.cs`.toLowerCase();
     const regex = new RegExp(str);
     return regex.test(uri.toLowerCase());
 }
