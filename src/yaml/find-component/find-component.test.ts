7dd9e1746945c303af62dc76991b8fc20fabe465
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-12 14:40:54 +0300

    use const where possible in tests

--- src/yaml/find-component/find-component.test.ts
+++ src/yaml/find-component/find-component.test.ts
@@ -1,23 +1,23 @@
 import assert from "assert";
 import { readFileSync } from "fs";
 import path from "path";
-import { findComponent } from "./find-component";
+import { findAllComponents, findComponent } from "./find-component";
 import { nonZeroBasedPosition } from "../../range/position";
 
 describe('find component in yaml document', () => {
 
     it('one entity - one component', () => {
-        let source = readSource('one-entity-one-component');
-        let okComponent = findComponent(source, nonZeroBasedPosition(3, 11));
+        const source = readSource('one-entity-one-component');
+        const okComponent = findComponent(source, nonZeroBasedPosition(3, 11));
         assert.ok(okComponent);
         assert.equal(okComponent.toString(), 'test');
-        let notComponent = findComponent(source, nonZeroBasedPosition(4, 11));
+        const notComponent = findComponent(source, nonZeroBasedPosition(4, 11));
         assert.equal(notComponent, undefined);
     });
 
     it('component bounds', () => {
-        let source = readSource('one-entity-one-component');
-        let positions = [
+        const source = readSource('one-entity-one-component');
+        const positions = [
             nonZeroBasedPosition(3, 10),
             nonZeroBasedPosition(3, 11),
             nonZeroBasedPosition(3, 12),
@@ -25,7 +25,7 @@ describe('find component in yaml document', () => {
             nonZeroBasedPosition(3, 14),
             nonZeroBasedPosition(3, 15)
         ];
-        let expect = [
+        const expect = [
             undefined,
             'test',
             'test',
@@ -33,24 +33,24 @@ describe('find component in yaml document', () => {
             'test',
             undefined
         ];
-        let components = positions.map(p => findComponent(source, p)?.toString());
+        const components = positions.map(p => findComponent(source, p)?.toString());
         assert.deepStrictEqual(components, expect);
     });
 
     it('no entity', () => {
-        let source = readSource('no-entity');
+        const source = readSource('no-entity');
         assert.equal(findComponent(source, nonZeroBasedPosition(3, 11)), undefined);
     });
 
     it('one entity - many components', () => {
-        let source = readSource('one-entity-many-components');
-        let comp1 = findComponent(source, nonZeroBasedPosition(3, 11));
+        const source = readSource('one-entity-many-components');
+        const comp1 = findComponent(source, nonZeroBasedPosition(3, 11));
         assert.ok(comp1);
         assert.equal(comp1.toString(), 'comp1');
-        let comp2 = findComponent(source, nonZeroBasedPosition(7, 11));
+        const comp2 = findComponent(source, nonZeroBasedPosition(7, 11));
         assert.ok(comp2);
         assert.equal(comp2.toString(), 'comp2');
-        let comp3 = findComponent(source, nonZeroBasedPosition(11, 11));
+        const comp3 = findComponent(source, nonZeroBasedPosition(11, 11));
         assert.ok(comp3);
         assert.equal(comp3.toString(), 'comp3');
     });
@@ -58,6 +58,6 @@ describe('find component in yaml document', () => {
 });
 
 function readSource(name: string): string {
-    let p = path.resolve(__dirname, `./test-resources/${name}.yml`);
+    const p = path.resolve(__dirname, `./test-resources/${name}.yml`);
     return readFileSync(p).toString();
 }
