2d965527647691eae3e3743cf7bad2ba082ea683
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-12 17:25:05 +0300

    simplify a bit

--- src/parse/summary.ts
+++ src/parse/summary.ts
@@ -1,7 +1,7 @@
 export function parseSummary(source: string, component: string): string | undefined {
     const docComment = '\\/\\/\\/\\s*';
     const matchAny = '[\\s\\S]*';
-    const regex = new RegExp(`(?:${docComment}<summary>\\s*(${matchAny})${docComment}<\\/summary>)${matchAny}${component}Component\\s*:\\s*Component`);
+    const regex = new RegExp(`${docComment}<summary>\\s*(${matchAny})${docComment}<\\/summary>${matchAny}${component}Component\\s*:\\s*Component`);
     const match = source.match(regex);
     if (match) {
         return match[1].replace(/^\/\/\/\s*/, '').replace(/\s*$/, '');
