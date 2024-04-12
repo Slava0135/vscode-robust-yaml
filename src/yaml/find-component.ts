994e84b38042ba94a256a4d06d17a9c897a6ec78
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-12 10:56:16 +0300

    pass test

--- src/yaml/find-component.ts
+++ src/yaml/find-component.ts
@@ -1,6 +1,26 @@
-import { Document, Scalar } from "yaml";
+import { Document, Scalar, isCollection, isScalar, visit } from "yaml";
 import { Position } from "../position/position";
 
 export function findComponent(doc: Document, pos: Position): Scalar | undefined {
-    return;
+    let component: Scalar | undefined;
+    visit(doc, {
+        Map(_, map) {
+            let components = map.get('components');
+            if (isCollection(components)) {
+                return visit(components, {
+                    Map(_, map) {
+                        let type = map.get('type', true);
+                        if (isScalar(type)) {
+                            component = type;
+                            return visit.BREAK;
+                        }
+                    }
+                });
+            }
+            if (component) {
+                return visit.BREAK;
+            }
+        }
+    });
+    return component;
 }
