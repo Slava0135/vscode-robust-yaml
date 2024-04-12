d108dc45281639f61723d20aa00f409913a572fd
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-12 18:29:02 +0300

    make hover work with files

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import { registerDefinitionProvider } from './definition-provider';
 import { registerDocumentSemanticTokensProvider } from './document-semantic-tokens-provider';
+import { registerHoverProvider } from './hover-provider';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"robust-yaml" is now active!');
 	context.subscriptions.push(registerDefinitionProvider());
 	context.subscriptions.push(registerDocumentSemanticTokensProvider());
+	context.subscriptions.push(registerHoverProvider());
 }
 
 export function deactivate() {
---
+++ src/hover-provider.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+import { findComponent } from './yaml/find-component/find-component';
+import { Position } from './range/position';
+import { containsComponentDefinition } from './file/uri';
+import { parseSummary } from './parse/summary';
+
+export function registerHoverProvider() {
+    return vscode.languages.registerHoverProvider('yaml', {
+        provideHover(document, position, token) {
+            const contents: vscode.MarkdownString[] = [];
+            const component = findComponent(document.getText(), new Position(position.line, position.character));
+            if (component) {
+                return vscode.workspace.findFiles("**/*Component.cs").then((componentFiles) => {
+                    const uri = componentFiles.find(uri => containsComponentDefinition(uri.toString(), component.toString()));
+                    contents.push(new vscode.MarkdownString(`#### ${component.toString()}`));
+                    if (uri) {
+                        return vscode.workspace.fs.readFile(uri);
+                    }
+                }).then(buf => {
+                    if (buf) {
+                        const summary = parseSummary(buf.toString(), component.toString());
+                        if (summary) {
+                            contents.push(new vscode.MarkdownString("```\n" + summary + "```"));
+                        }
+                    }
+                    return {
+                        contents: contents
+                    };
+                });
+            }
+        }
+    });
+}
