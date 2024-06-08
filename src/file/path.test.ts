75edd48dea955e4ce300740579cd3a15983a486e
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-06-08 17:51:48 +0300

    complex test for path

--- src/file/path.test.ts
+++ src/file/path.test.ts
@@ -2,9 +2,15 @@ import assert from "assert";
 import { containsComponentDefinition } from "./path";
 
 describe('path', () => {
+
     it('contains component definition', () => {
         assert.ok(containsComponentDefinition('/path/to/a/SomeTestComponent.cs', 'SomeTest'));
         assert.ok(containsComponentDefinition('/path/to/a/SharedSomeTestComponent.cs', 'SomeTest'));
         assert.equal(containsComponentDefinition('/path/to/a/SomeTest/OtherComponent.cs', 'SomeTest'), false);
     });
+
+    it('contains component definition (uri)', () => {
+        assert.ok(containsComponentDefinition('file:///home/user/project/Content.Shared/Item/ItemComponent.cs', 'Item'));
+    });
+
 });
