1e2c1cc368c9b96c34863bdc9be79b7f0e2826a9
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-07-04 11:28:35 +0300

    provide hover for datafields

--- src/hover-provider.ts
+++ src/hover-provider.ts
@@ -1,17 +1,19 @@
 import * as vscode from 'vscode';
-import { findComponent } from './yaml/find/find';
+import { findAllFields, findComponent, findComponentByField, findField, isAtComponentField } from './yaml/find/find';
 import { Position } from './range/position';
 import { containsComponentDefinition } from './file/path';
-import { parseComponentSummary } from './parse/summary';
+import { parseComponentSummary, parseDatafieldSummary } from './parse/summary';
 import { getComponentUris } from './uri-store';
 import { logger } from './logging';
+import { parseDataFields } from './parse/datafield';
 
 export function registerHoverProvider() {
     return vscode.languages.registerHoverProvider('yaml', {
         provideHover(document, position, _token) {
             logger.debug(`providing hover in ${document.uri.toString()} at ${position.line}:${position.character}`);
             const contents: vscode.MarkdownString[] = [];
-            const component = findComponent(document.getText(), new Position(position.line, position.character));
+            const pos = new Position(position.line, position.character);
+            const component = findComponent(document.getText(), pos);
             if (component) {
                 const uri = getComponentUris().find(uri => containsComponentDefinition(uri.toString(), component.toString()));
                 if (uri) {
@@ -30,6 +32,34 @@ export function registerHoverProvider() {
                 } else {
                     contents.push(new vscode.MarkdownString(`???`));
                 }
+                return {
+                    contents: contents
+                };
+            } else if (isAtComponentField(document.getText(), pos)) {
+                const componentName = findComponentByField(document.getText(), pos);
+                const datafieldName = findField(document.getText(), pos);
+                contents.push(new vscode.MarkdownString('**???**'));
+                if (componentName && datafieldName) {
+                    const uri = getComponentUris().find(uri => containsComponentDefinition(uri.toString(), componentName));
+                    if (uri) {
+                        return vscode.workspace.fs.readFile(uri).then(buf => {
+                            if (buf) {
+                                const datafield = parseDataFields(buf.toString()).find(it => it.name === datafieldName);
+                                if (datafield) {
+                                    contents.pop();
+                                    contents.push(new vscode.MarkdownString(`**${datafield.type ?? '???'}**`));
+                                    const summary = parseDatafieldSummary(buf.toString(), datafieldName);
+                                    if (summary) {
+                                        contents.push(new vscode.MarkdownString("```xml\n" + summary + "\n```"));
+                                    }
+                                }
+                                return {
+                                    contents: contents
+                                };
+                            }
+                        });
+                    }
+                } 
                 return {
                     contents: contents
                 };
