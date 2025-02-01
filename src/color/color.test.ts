0a023448c5f92fc651fbfbec2f549090b48ba03b
slava0135 <super.novalskiy_0135@inbox.ru>
2025-02-01 17:25:55 +0300

    feat: hexToColor

--- src/color/color.test.ts
+++ src/color/color.test.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { isHexColor } from "./color";
+import { Color, hexToColor, isHexColor } from "./color";
 
 describe('colors', () => {
 
@@ -9,6 +9,13 @@ describe('colors', () => {
         assert.equal(isHexColor("123456"), false);
         assert.equal(isHexColor("#123abce"), false);
         assert.equal(isHexColor("#afafah"), false);
+    });
+
+    it('hex to color', () => {
+        assert.equal(hexToColor("123456"), undefined);
+        const actual = hexToColor("#1AFFa1");
+        const expected = new Color(26, 255, 161);
+        assert.ok(actual !== undefined && expected.equals(actual));
     });
 
 });
--- src/color/color.ts
+++ src/color/color.ts
@@ -1,5 +1,32 @@
 const hexRegex = new RegExp(/^#([A-Fa-f0-9]{6})$/);
 
+export class Color {
+
+    readonly red: number;
+    readonly green: number;
+    readonly blue: number;
+
+    constructor(red: number, green: number, blue: number) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public equals(other: Color): boolean {
+        return this.red === other.red && this.green === other.green && this.blue === other.blue;
+    }
+
+}
+
 export function isHexColor(str: string): boolean {
     return hexRegex.test(str);
+}
+
+export function hexToColor(str: string): Color | undefined {
+    if (isHexColor(str)) {
+        const red = parseInt(str.slice(1, 2 + 1), 16);
+        const blue = parseInt(str.slice(3, 4 + 1), 16);
+        const green = parseInt(str.slice(5, 6 + 1), 16);
+        return new Color(red, blue, green);
+    }
 }
