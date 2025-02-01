ee2d5c912aa49415db5bebc7488624b66fd5d667
slava0135 <super.novalskiy_0135@inbox.ru>
2025-02-01 15:46:07 +0300

    style: fix more indentation

--- src/definition-provider.ts
+++ src/definition-provider.ts
@@ -8,70 +8,70 @@ import { parseDataFields } from './parse/datafield';
 import { Scalar } from 'yaml';
 
 export function registerDefinitionProvider(): vscode.Disposable {
-	return vscode.languages.registerDefinitionProvider('yaml', {
-		provideDefinition(document, position, _token) {
-			logger.debug(`providing definition in ${document.uri.toString()} at ${position.line}:${position.character}`);
-			const locations: vscode.Location[] = [];
-			const pos = new Position(position.line, position.character);
-			const component = findComponent(document.getText(), pos);
-			if (component) {
-				provideComponentDefinition(component, locations);
-			} else if (isAtComponentField(document.getText(), pos)) {
-				logger.debug(`finding datafield definition...`);
-				const componentName = findComponentByField(document.getText(), pos);
-				const datafieldName = findField(document.getText(), pos);
-				if (componentName && datafieldName) {
-					const components = getComponentUris().filter(uri => containsComponentDefinition(uri.toString(), componentName.toString()));
-					if (components.length === 1) {
-						let uri = components[0];
-						return provideDataFieldDefinition(uri, datafieldName, locations);
-					}
-				}
-			} else if (findPath(document.getText(), pos)) {
-				logger.debug(`finding path definition...`);
-				let path = findPath(document.getText(), pos)!!;
-				if (path?.startsWith('/')) {
-					path = path.slice(1);
-				}
-				logger.debug(`searching files with path '${path}'`);
-				return findFiles(path, locations);
-			}
-			return locations;
-		}
-	});
+    return vscode.languages.registerDefinitionProvider('yaml', {
+        provideDefinition(document, position, _token) {
+            logger.debug(`providing definition in ${document.uri.toString()} at ${position.line}:${position.character}`);
+            const locations: vscode.Location[] = [];
+            const pos = new Position(position.line, position.character);
+            const component = findComponent(document.getText(), pos);
+            if (component) {
+                provideComponentDefinition(component, locations);
+            } else if (isAtComponentField(document.getText(), pos)) {
+                logger.debug(`finding datafield definition...`);
+                const componentName = findComponentByField(document.getText(), pos);
+                const datafieldName = findField(document.getText(), pos);
+                if (componentName && datafieldName) {
+                    const components = getComponentUris().filter(uri => containsComponentDefinition(uri.toString(), componentName.toString()));
+                    if (components.length === 1) {
+                        let uri = components[0];
+                        return provideDataFieldDefinition(uri, datafieldName, locations);
+                    }
+                }
+            } else if (findPath(document.getText(), pos)) {
+                logger.debug(`finding path definition...`);
+                let path = findPath(document.getText(), pos)!!;
+                if (path?.startsWith('/')) {
+                    path = path.slice(1);
+                }
+                logger.debug(`searching files with path '${path}'`);
+                return findFiles(path, locations);
+            }
+            return locations;
+        }
+    });
 }
 
 function findFiles(path: string, locations: vscode.Location[]): vscode.ProviderResult<vscode.Definition | vscode.LocationLink[]> {
-	return vscode.workspace.findFiles(`{**/${path},**/${path}/**}`).then(uris => {
-		uris.forEach(it => {
-			const uri = it.toString();
-			if (uri.endsWith(path)) {
-				locations.push(new vscode.Location(it, new vscode.Position(0, 0)));
-			} else {
-				const start = uri.lastIndexOf(path);
-				locations.push(new vscode.Location(vscode.Uri.parse(uri.slice(0, start + path.length)), new vscode.Position(0, 0)));
-			}
-		});
-		return locations;
-	});
+    return vscode.workspace.findFiles(`{**/${path},**/${path}/**}`).then(uris => {
+        uris.forEach(it => {
+            const uri = it.toString();
+            if (uri.endsWith(path)) {
+                locations.push(new vscode.Location(it, new vscode.Position(0, 0)));
+            } else {
+                const start = uri.lastIndexOf(path);
+                locations.push(new vscode.Location(vscode.Uri.parse(uri.slice(0, start + path.length)), new vscode.Position(0, 0)));
+            }
+        });
+        return locations;
+    });
 }
 
 function provideDataFieldDefinition(uri: vscode.Uri, datafieldName: string, locations: vscode.Location[]): vscode.ProviderResult<vscode.Definition | vscode.LocationLink[]> {
-	return vscode.workspace.fs.readFile(uri).then(buf => {
-		if (buf) {
-			const datafields = parseDataFields(buf.toString());
-			const datafield = datafields.find(it => it.name === datafieldName);
-			if (datafield) {
-				locations.push(new vscode.Location(uri, new vscode.Position(datafield.line - 1, 0)));
-			}
-			return locations;
-		}
-	});
+    return vscode.workspace.fs.readFile(uri).then(buf => {
+        if (buf) {
+            const datafields = parseDataFields(buf.toString());
+            const datafield = datafields.find(it => it.name === datafieldName);
+            if (datafield) {
+                locations.push(new vscode.Location(uri, new vscode.Position(datafield.line - 1, 0)));
+            }
+            return locations;
+        }
+    });
 }
 
 function provideComponentDefinition(component: Scalar, locations: vscode.Location[]) {
-	logger.debug(`finding component definition...`);
-	getComponentUris()
-		.filter(uri => containsComponentDefinition(uri.toString(), component.toString()))
-		.forEach(uri => locations.push(new vscode.Location(uri, new vscode.Position(0, 0))));
+    logger.debug(`finding component definition...`);
+    getComponentUris()
+        .filter(uri => containsComponentDefinition(uri.toString(), component.toString()))
+        .forEach(uri => locations.push(new vscode.Location(uri, new vscode.Position(0, 0))));
 }
--- src/extension.ts
+++ src/extension.ts
@@ -9,28 +9,28 @@ import * as logging from './logging';
 import * as configuration from './configuration';
 
 export function activate(context: vscode.ExtensionContext) {
-	initLogging(context);
-	logger.info("activating extension...");
-	uristore.init().forEach(it => context.subscriptions.push(it));
-	context.subscriptions.push(registerDefinitionProvider());
-	context.subscriptions.push(registerDocumentSemanticTokensProvider());
-	context.subscriptions.push(registerHoverProvider());
-	context.subscriptions.push(registerCompletionItemProvider());
-	logger.info("extension activated!");
+    initLogging(context);
+    logger.info("activating extension...");
+    uristore.init().forEach(it => context.subscriptions.push(it));
+    context.subscriptions.push(registerDefinitionProvider());
+    context.subscriptions.push(registerDocumentSemanticTokensProvider());
+    context.subscriptions.push(registerHoverProvider());
+    context.subscriptions.push(registerCompletionItemProvider());
+    logger.info("extension activated!");
 }
 
 function initLogging(context: vscode.ExtensionContext) {
-	function setLogLevel() {
-		let logLevel = configuration.getLogLevel();
-		logging.setLevel(logLevel);
-	}
-	logging.init(vscode.window.createOutputChannel("Robust YAML", "log"));
-	setLogLevel();
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeConfiguration((e) => {
-			if (configuration.didAffectLogLevel(e)) {
-				setLogLevel();
-			}
-		})
-	);
+    function setLogLevel() {
+        let logLevel = configuration.getLogLevel();
+        logging.setLevel(logLevel);
+    }
+    logging.init(vscode.window.createOutputChannel("Robust YAML", "log"));
+    setLogLevel();
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration((e) => {
+            if (configuration.didAffectLogLevel(e)) {
+                setLogLevel();
+            }
+        })
+    );
 }
