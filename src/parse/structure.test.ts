c688bc050f822ea7864c25238d904e982d829d70
slava0135 <super.novalskiy_0135@inbox.ru>
2026-06-22 10:37:26 +0300

    style: fix indent

--- src/parse/structure.test.ts
+++ src/parse/structure.test.ts
@@ -5,35 +5,35 @@ import { parseStructures, Structure, StructureKind } from "./structure";
 
 describe('parse structure', () => {
 
-  it('one component', () => {
-    const source = readSource('one-component');
-    assert.deepEqual(parseStructures(source), [new Structure(StructureKind.Component, "SomeComponent", 9, 12)]);
-  });
+    it('one component', () => {
+        const source = readSource('one-component');
+        assert.deepEqual(parseStructures(source), [new Structure(StructureKind.Component, "SomeComponent", 9, 12)]);
+    });
 
-  it('two components', () => {
-    const source = readSource('two-components');
-    assert.deepEqual(parseStructures(source), [
-      new Structure(StructureKind.Component, "ComponentA", 9, 12),
-      new Structure(StructureKind.Component, "ComponentB", 18, 21)
-    ]);
-  });
+    it('two components', () => {
+        const source = readSource('two-components');
+        assert.deepEqual(parseStructures(source), [
+            new Structure(StructureKind.Component, "ComponentA", 9, 12),
+            new Structure(StructureKind.Component, "ComponentB", 18, 21)
+        ]);
+    });
 
-  it('no components', () => {
-    const source = readSource('no-components');
-    assert.deepEqual(parseStructures(source), []);
-  });
+    it('no components', () => {
+        const source = readSource('no-components');
+        assert.deepEqual(parseStructures(source), []);
+    });
 
-  it('nested components', () => {
-    const source = readSource('nested-components');
-    assert.deepEqual(parseStructures(source), [
-      new Structure(StructureKind.Component, "ComponentA", 9, 21),
-      new Structure(StructureKind.Component, "ComponentB", 17, 20)
-    ]);
-  });
+    it('nested components', () => {
+        const source = readSource('nested-components');
+        assert.deepEqual(parseStructures(source), [
+            new Structure(StructureKind.Component, "ComponentA", 9, 21),
+            new Structure(StructureKind.Component, "ComponentB", 17, 20)
+        ]);
+    });
 
 });
 
 function readSource(name: string): string {
-  const p = path.resolve(__dirname, `./test-resources/structure/${name}.cs`);
-  return readFileSync(p).toString();
+    const p = path.resolve(__dirname, `./test-resources/structure/${name}.cs`);
+    return readFileSync(p).toString();
 }
